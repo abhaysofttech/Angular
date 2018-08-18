263494ea6ea92f0801ab5f9571123e1fc943cf21
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-18 16:42:02 +0530

    for loop is TS using let Keyword

--- ts-hello/declareVaiable.js
+++ ts-hello/declareVaiable.js
@@ -4,4 +4,11 @@ function doSomething() {
     }
     console.log('Finally: ' + i);
 }
+function doSomethingElse() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i); // even if use let keyword TS witl give the compile time error.
+}
 doSomething();
+doSomethingElse();
--- ts-hello/declareVaiable.ts
+++ ts-hello/declareVaiable.ts
@@ -1,9 +1,18 @@
-function doSomething(){
-    for(var i=0; i<5;i++){
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
         console.log(i);
     }
 
     console.log('Finally: ' + i);
 }
 
+function doSomethingElse() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log('Finally: ' + i); // even if use let keyword TS witl give the compile time error.
+}
+
 doSomething();
+doSomethingElse();
