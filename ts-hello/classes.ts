4614cc854611a8b066fafbcef88985b912ae1b2b
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-18 20:35:23 +0530

    create a class function in TS

---
+++ ts-hello/classes.ts
@@ -0,0 +1,13 @@
+
+// insted of this we ca use the interface
+// Note First Letter of every interface shoud ne Capital Letter
+class Point {
+    x: number;
+    y: number;
+    draw() {
+
+    }
+    getDistance(another:Point){
+
+    }
+}
