5e1482c9a29ba456bccd4319209b2505366f3f09
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-18 20:28:53 +0530

    using the INTERFACE in TS

--- ts-hello/interface.js
+++ ts-hello/interface.js
@@ -7,3 +7,10 @@ drawPoint({
     x: 1,
     y: 2
 });
+var newDrawPoint = function (point) {
+    console.log(point);
+};
+newDrawPoint({
+    x: 1,
+    y: 2
+});
--- ts-hello/interface.ts
+++ ts-hello/interface.ts
@@ -10,3 +10,19 @@ drawPoint({
     x: 1,
     y: 2
 })
+
+// insted of this we ca use the interface
+// Note First Letter of every interface shoud ne Capital Letter
+interface Point {
+    x: number,
+    y: number
+}
+let newDrawPoint = (point: Point) => {
+    console.log(point)
+}
+
+newDrawPoint({
+    x: 1,
+    y: 2
+
+})
