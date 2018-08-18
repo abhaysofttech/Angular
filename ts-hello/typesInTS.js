3ecce0077f79c316b69bd34cfab3b5b45c68a1d5
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-18 19:58:11 +0530

    Defferent type of Types in TS

--- ts-hello/typesInTS.js
+++ ts-hello/typesInTS.js
@@ -1,2 +1,11 @@
 var count = 5;
 count = 'a'; // we will get the compile type error
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
--- ts-hello/typesInTS.ts
+++ ts-hello/typesInTS.ts
@@ -1,2 +1,14 @@
 let count = 5;
 count = 'a';  // we will get the compile type error
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a',false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
