15b4d190e54a15d32a05e299c2e6b9610cf9e90f
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 10:33:49 +0530

    ngFor Directive

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `<h2>{{title}}</h2>
-                {{getTitle()}}`
+               <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+               </ul>`
 })
 export class CoursesComponent {
     title = "List of Courses"
-    
-    getTitle(){
-        return this.title;
-    }
+
+    courses = ["course 1", "course 2", "course 3"]
 }
