52cdf2a53844a4bbc7c8c199ce20cb1e5a066e64
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 12:14:08 +0530

    Style Binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
     selector: 'courses',
     template: `
             <button class="btn btn-primary" [class.active]="isActive">Save</button>
-               `
+             <button class="btn btn-primary" [style.backgroundColor]="isActive?'blue':'white'">Save 2</button>  `
 })
 export class CoursesComponent {
- isActive = false;
+    isActive = false;
 }
