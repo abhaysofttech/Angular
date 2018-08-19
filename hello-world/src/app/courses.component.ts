82900fb7bf6cc6902ae960d83303d6835cc75aca
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 12:09:20 +0530

    Adding bootstrap and Class Binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,33 +3,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{title}}</h2>
-               <ul>
-                    <li *ngFor="let course of courses">
-                        {{course}}
-                    </li>
-               </ul>
-               <h2 [textContent] = "title"></h2>
-               <img src="{{ imageUrl}}" />
-
-               <table>
-               <tr>
-               <td [attr.colspan]="colSpan">Data</td>
-               </tr>
-               </table>
+    template: `
+            <button class="btn btn-primary" [class.active]="isActive">Save</button>
                `
 })
 export class CoursesComponent {
-    title = "List of Courses"
-    courses;
-    imageUrl = "http://lorempixel.com/400/200";
-    colSpan = 2;
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-        
-    }
-
-    //Logic for calling  an HTTP Service
-
+ isActive = false;
 }
