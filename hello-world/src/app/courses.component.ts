08923dead08037a6007341bfcd8a0d8fc389ba21
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 11:54:52 +0530

    Property binding & Attribute binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
                     <li *ngFor="let course of courses">
                         {{course}}
                     </li>
-               </ul>`
+               </ul>
+               <h2 [textContent] = "title"></h2>
+               <img src="{{ imageUrl}}" />
+
+               <table>
+               <tr>
+               <td [attr.colspan]="colSpan">Data</td>
+               </tr>
+               </table>
+               `
 })
 export class CoursesComponent {
     title = "List of Courses"
     courses;
+    imageUrl = "http://lorempixel.com/400/200";
+    colSpan = 2;
 
     constructor(service: CoursesService){
         this.courses = service.getCourses();
