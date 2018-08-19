c6c920d511c301a22e2c820051984cff8de41db5
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 10:54:14 +0530

    Dependency Injection Services
    Angular take care to inser the new service object -- Dependency Injection

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -14,9 +14,9 @@ export class CoursesComponent {
     title = "List of Courses"
     courses;
 
-    constructor(){
-        let service = new CoursesService();
+    constructor(service: CoursesService){
         this.courses = service.getCourses();
+        
     }
 
     //Logic for calling  an HTTP Service
