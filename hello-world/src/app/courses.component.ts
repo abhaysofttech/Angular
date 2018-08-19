587a55c2bc2435f28e5e082fe254cfa52755f3bd
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-19 10:29:36 +0530

    method call using interpolation

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{title}}</h2>'
+    template: `<h2>{{title}}</h2>
+                {{getTitle()}}`
 })
 export class CoursesComponent {
     title = "List of Courses"
     
+    getTitle(){
+        return this.title;
+    }
 }
