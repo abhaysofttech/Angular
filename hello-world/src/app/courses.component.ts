3b463abf8ccacb8864a7ab9502170566d0591f59
Abhay Narnaware <abhaysofttech@gmail.com>
2018-08-28 13:53:31 +0530

    updat the hello word

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
     selector: 'courses',
     template: `
             <button class="btn btn-primary" [class.active]="isActive">Save</button>
-             <button class="btn btn-primary" [style.backgroundColor]="isActive?'blue':'white'">Save 2</button>  `
+             <button class="btn btn-primary" [style.backgroundColor]="isActive?'blue':'white'">Save 2</button>  
+             <button (click)="onSave()">Click Save</button>
+             `
 })
 export class CoursesComponent {
     isActive = false;
+    onSave(){
+        console.log("Button was clicked")
+    }
 }
