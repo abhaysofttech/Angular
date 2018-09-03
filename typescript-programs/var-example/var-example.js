b74d3a4e096531b0c6f991a1f4cd1eb57487b307
Abhay Narnaware <abhaysofttech@gmail.com>
2018-09-03 11:46:12 +0530

    first Commit

---
+++ typescript-programs/var-example/var-example.js
@@ -0,0 +1,18 @@
+function doSomething() {
+    // used the var for in a for loop
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // print the final value of i
+    console.log('Finally value of i :-' + i);
+}
+doSomething();
+// run the command to comiple ts --------------- tsc var-example.ts
+//run the comment to see the below Output ------ node var-example.js
+// Output ************
+// 0
+// 1
+// 2
+// 3
+// 4
+// Finally value of i :-5
