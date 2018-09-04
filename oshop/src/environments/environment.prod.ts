63f3b876fe75ef97eb5f458a860a39dce81d2ce7
Abhay Narnaware <abhaysofttech@gmail.com>
2018-09-04 19:58:49 +0530

    setup new oshop project branch

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAA2M6eCdi1G6pKDpMR_RHnY2a8YyBm2RY",
+    authDomain: "oshop-fcc4f.firebaseapp.com",
+    databaseURL: "https://oshop-fcc4f.firebaseio.com",
+    projectId: "oshop-fcc4f",
+    storageBucket: "",
+    messagingSenderId: "149556553393"
+  }
+};
---
+++ oshop/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAA2M6eCdi1G6pKDpMR_RHnY2a8YyBm2RY",
+    authDomain: "oshop-fcc4f.firebaseapp.com",
+    databaseURL: "https://oshop-fcc4f.firebaseio.com",
+    projectId: "oshop-fcc4f",
+    storageBucket: "",
+    messagingSenderId: "149556553393"
+  }
+};
+
+/*
+ * In development mode, for easier debugging, you can ignore zone related error
+ * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
+ * below file. Don't forget to comment it out in production mode
+ * because it will have a performance impact when errors are thrown
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
